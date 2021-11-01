13e7d2051986f776b6e35a6b4e3bd5c51ff646bf
i-yuuki <i-yuuki@users.noreply.github.com>
2021-11-01 17:44:29 +0900

    プレイヤーHP表示を追加、Prefab化

---
+++ Assets/Scripts/UI/PlayerHealthDisplay.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using Echo.Extensions;
+using Echo.Player;
+
+namespace Echo.UI{
+    public class PlayerHealthDisplay : MonoBehaviour{
+        
+        [SerializeField] private float spriteSize;
+        [SerializeField] PlayerBase player;
+        [SerializeField] Slider slider;
+
+        public PlayerBase Player{
+            set => player = value;
+        }
+
+        private void Start(){
+            slider.maxValue = player.MaxHealth;
+            var t = slider.transform as RectTransform;
+            t.sizeDelta = t.sizeDelta.WithX(spriteSize * player.MaxHealth);
+
+            player.OnHealthChange.Subscribe(health => slider.value = health).AddTo(this);
+        }
+
+    }
+}
