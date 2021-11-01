4cdfcd982f7b0bf8ffe9dfa89f58c8d4092e9b12
i-yuuki <i-yuuki@users.noreply.github.com>
2021-11-01 12:43:57 +0900

    Baked Lighting 削除、タイトルシーン試作追加

---
+++ Assets/Scripts/Misc/LoadSceneOnTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Echo.Misc{
+    public sealed class LoadSceneOnTrigger : MonoBehaviour{
+
+        [SerializeField] private string sceneName;
+
+        private void OnTriggerEnter(Collider other){
+            if(other.CompareTag("Player")){
+                SceneManager.LoadScene(sceneName);
+            }
+        }
+
+    }
+}
