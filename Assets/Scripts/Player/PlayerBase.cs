5be4038194060e3219bba6e86ba9c6e977a4ba34
i-yuuki <i-yuuki@users.noreply.github.com>
2021-11-01 16:24:46 +0900

    プレイヤーにHPを追加

--- Assets/Scripts/Player/PlayerBase.cs
+++ Assets/Scripts/Player/PlayerBase.cs
@@ -1,9 +1,24 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UniRx;
 
 namespace Echo.Player{
     public class PlayerBase : MonoBehaviour{
 
+        [SerializeField] private IntReactiveProperty health;
+        [SerializeField] private int maxHealth;
         [SerializeField] private PlayerReflectBullet reflectBullet;
+
+        public int Health{
+            get => health.Value;
+            private set => health.Value = value;
+        }
+        public int MaxHealth => maxHealth;
+        public IObservable<int> OnHealthChange => health;
+
+        public void Damage(int damage){
+            Health -= damage;
+        }
 
         void Start(){
             reflectBullet?.Init(this);
