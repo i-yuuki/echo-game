bfd08272631930801bdd15d9a6207115a641a21a
i-yuuki <i-yuuki@users.noreply.github.com>
2021-11-03 17:54:19 +0900

    (仮) タイトルUIを追加

---
+++ Assets/Scripts/Scene/Title/TitleScene.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Echo.Player;
+
+namespace Echo.Scene.Title{
+    public sealed class TitleScene : MonoBehaviour{
+
+        [SerializeField] private CanvasGroup container;
+        [SerializeField] private Button buttonPlay;
+        [SerializeField] private Button buttonSettings;
+        [SerializeField] private Button buttonQuit;
+        [SerializeField] private PlayerMovement playerMovement;
+
+        void Start(){
+            playerMovement.enabled = false;
+            buttonPlay.onClick.AddListener(() => {
+                playerMovement.enabled = true;
+                Hide();
+            });
+            buttonSettings.onClick.AddListener(() => {
+                // TODO show settings
+                Hide();
+            });
+            buttonQuit.onClick.AddListener(() => Application.Quit());
+        }
+
+        private void Hide(){
+            container.interactable = false;
+            // TODO fade out
+            container.gameObject.SetActive(false);
+        }
+
+    }
+}
