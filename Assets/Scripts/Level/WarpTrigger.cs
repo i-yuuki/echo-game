ad6dabffc55b729b35c7dd07cce0a47edc11fb19
i-yuuki <i-yuuki@users.noreply.github.com>
2021-11-04 22:11:16 +0900

    部屋移動を追加

---
+++ Assets/Scripts/Level/WarpTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Echo.Level{
+    public sealed class WarpTrigger : MonoBehaviour{
+
+        [SerializeField] private Transform exit;
+
+        private void OnTriggerEnter(Collider other){
+            if(!other.CompareTag("Player")) return;
+            var characterController = other.GetComponent<CharacterController>();
+            if(characterController) characterController.enabled = false;
+            other.transform.position = exit.position;
+            if(characterController) characterController.enabled = true;
+        }
+
+    }
+}
