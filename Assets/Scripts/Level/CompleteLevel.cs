2f74aae03425b1c30b1f7ab9e0cfb9de66c5d3d0
i-yuuki <i-yuuki@users.noreply.github.com>
2021-11-06 13:24:48 +0900

    (仮) レベルクリア表示を追加

---
+++ Assets/Scripts/Level/CompleteLevel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using Cinemachine;
+using Cysharp.Threading.Tasks;
+using UniRx;
+using Echo.UI;
+
+namespace Echo.Level{
+    public sealed class CompleteLevel : MonoBehaviour{
+
+        [SerializeField] private float slowmoDuration;
+        [SerializeField] private RoomEnemies enemies;
+        [SerializeField] private CinemachineVirtualCamera virtualCamera;
+        [SerializeField] private LevelCompleteDisplay display;
+
+        private void Start(){
+            enemies.OnAllEnemiesDied.Subscribe(_ => Display().Forget()).AddTo(this);
+        }
+
+        private async UniTaskVoid Display(){
+            // TODO disable gameplay input
+            Time.timeScale = 0.5f;
+            await UniTask.Delay(TimeSpan.FromSeconds(slowmoDuration), ignoreTimeScale: true);
+            Time.timeScale = 1;
+            virtualCamera.gameObject.SetActive(true);
+            await UniTask.Delay(TimeSpan.FromSeconds(0.5f));
+            display.Show();
+        }
+
+    }
+}
---
+++ Assets/Scripts/UI/LevelCompleteDisplay.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+
+namespace Echo.UI{
+    public sealed class LevelCompleteDisplay : MonoBehaviour{
+
+        [SerializeField] private CanvasGroup container;
+
+        public void Show(){
+            ShowAsync().Forget();
+        }
+
+        public async UniTask ShowAsync(){
+            gameObject.SetActive(true);
+            await container.DOFade(1, 0.3f).From(0);
+        }
+
+    }
+}
