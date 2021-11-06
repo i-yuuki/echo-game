ad48435273595dda4d294de3317459611fb1f5b2
i-yuuki <i-yuuki@users.noreply.github.com>
2021-11-06 18:50:51 +0900

    (超仮) レベル選択画面を追加

---
+++ Assets/Scripts/Scene/LevelSelect/LevelSelectScene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Echo.Scene.LevelSelect{
+    public sealed class LevelSelectScene : MonoBehaviour{
+
+        [SerializeField] private Button buttonConfirm;
+
+        private void Start(){
+            buttonConfirm.onClick.AddListener(() => GameManager.Instance.LoadScene("Level1"));
+        }
+
+    }
+}
