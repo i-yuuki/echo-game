62a4ded79a49af7726c5e27bf324d064e3894b6f
i-yuuki <i-yuuki@users.noreply.github.com>
2021-11-11 14:05:45 +0900

    エコーちょっと実装＆エコー部屋ちょっと作る

---
+++ Assets/Scripts/Boss/Echo.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using UnityEngine;
+using DG.Tweening;
+using UniRx;
+using Echo.Enemy;
+using Echo.Extensions;
+using Echo.Player;
+
+namespace Echo.Boss{
+    public sealed class Echo : MonoBehaviour, IPlayerBulletAttackable{
+
+        [SerializeField] private EnemyBase enemy;
+        [SerializeField] private EnemySensePlayer enemySensePlayer;
+        [SerializeField] private float attackInterval;
+        [SerializeField] private int bulletCount;
+        [SerializeField] private float bulletSpread;
+        [SerializeField] private float bulletSpeed;
+        [SerializeField] private Transform shootPosition;
+        [SerializeField] private Animator animator;
+        [SerializeField] private EnemyBullet bulletPrefab;
+
+        private void Start(){
+            enemySensePlayer.OnPlayerFound.Subscribe(_ => SetEnemyAwake(true)).AddTo(this);
+            enemySensePlayer.OnPlayerLost.Subscribe(_ => SetEnemyAwake(false)).AddTo(this);
+        }
+
+        private void SetEnemyAwake(bool awake){
+            StopCoroutine("AttackLoop");
+            if(awake){
+                StartCoroutine("AttackLoop");
+            }
+        }
+
+        private IEnumerator AttackLoop(){
+            while(true){
+                yield return new WaitForSeconds(attackInterval);
+                transform.DODynamicLookAt(enemySensePlayer.PlayerNearby.transform.position.WithY(transform.position.y), 0.5f);
+                animator.SetTrigger("Attack");
+            }
+        }
+
+        // Animation Event から呼ばれる
+        public void Attack(){
+            var playerPos = enemySensePlayer.PlayerNearby.transform.position;
+            var direction = (playerPos - shootPosition.position).WithY(0);
+            for(int i = 0;i < bulletCount;i ++){
+                var bullet = Instantiate(bulletPrefab, shootPosition.position, Quaternion.identity);
+                bullet.Shooter = transform;
+                bullet.Direction = Quaternion.AngleAxis((i - bulletCount / 2.0f) * bulletSpread, Vector3.up) * direction;
+                bullet.Speed = bulletSpeed;
+            }
+        }
+        
+        void IPlayerBulletAttackable.OnPlayerBulletHit(PlayerBullet bullet){
+            enemy.Damage(1);
+            animator.SetTrigger("Damage");
+        }
+
+    }
+}
