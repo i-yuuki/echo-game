4815ecbdbe3870bd983170f42cbd400fb5732f35
i-yuuki <i-yuuki@users.noreply.github.com>
2021-11-11 13:19:43 +0900

    ステージ1の配置

--- Assets/Scripts/Level/RoomDoor.cs
+++ Assets/Scripts/Level/RoomDoor.cs
@@ -4,9 +4,11 @@ namespace Echo.Level{
     public class RoomDoor : MonoBehaviour{
 
         [SerializeField] private Animator animator;
+        [SerializeField] private new Collider collider;
 
         public void Open(){
             animator.SetTrigger("Open");
+            collider.enabled = false;
         }
 
     }
