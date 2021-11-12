ead29d8f5e9a07c47558d5ca4b64719574a718d3
i-yuuki <i-yuuki@users.noreply.github.com>
2021-11-12 18:14:08 +0900

    セーブのようななにか

---
+++ Assets/Scripts/SaveData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace Echo{
+    // 仮セーブ
+    [Serializable]
+    public sealed class SaveData{
+
+        private static string FilePath = Application.persistentDataPath + "/beta-progress.json";
+
+        public static SaveData Instance{ get; private set; } = new SaveData();
+
+        public int levelsCompleted = 0;
+        public int health = 4;
+        public int maxHealth = 4;
+
+        public static void Load(){
+            try{
+                using(var stream = new StreamReader(FilePath)){
+                    Instance = JsonUtility.FromJson<SaveData>(stream.ReadToEnd());
+                }
+            }catch(Exception ex){
+                Instance = new SaveData();
+                Debug.LogError($"Failed to load game progress! {ex}");
+            }
+        }
+
+        public static void Save(){
+            try{
+                using(var stream = new StreamWriter(FilePath)){
+                    stream.Write(JsonUtility.ToJson(Instance));
+                }
+            }catch(Exception ex){
+                Instance = new SaveData();
+                Debug.LogError($"Failed to save game progress! {ex}");
+            }
+        }
+
+    }
+}
