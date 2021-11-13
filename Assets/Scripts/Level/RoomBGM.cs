a9f334ccee95b00fe79f41eb6210344e1ff35b62
i-yuuki <i-yuuki@users.noreply.github.com>
2021-11-13 10:48:33 +0900

    ボスBGMを追加

---
+++ Assets/Scripts/Level/RoomBGM.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UniRx;
+using Echo.Audio;
+
+namespace Echo.Level{
+    public sealed class RoomBGM : MonoBehaviour{
+
+        [SerializeField] private AudioCue bgm;
+        [SerializeField] private AudioChannel channel;
+        [SerializeField] private Room room;
+        [SerializeField] private bool stopOnComplete;
+
+        private void Start(){
+            room.OnEnter.Subscribe(_ => channel.Request(bgm)).AddTo(this);
+            if(stopOnComplete){
+                room.OnComplete.Subscribe(_ => channel.RequestStop()).AddTo(this);
+            }
+        }
+
+    }
+}
