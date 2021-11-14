1ab3459ebab21b3106dc8ba4011235dbfdd13a39
i-yuuki <i-yuuki@users.noreply.github.com>
2021-11-14 23:31:34 +0900

    (仮) プロローグを追加

---
+++ Assets/Scripts/Scene/PrologueScene.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+using Cysharp.Threading.Tasks;
+using Echo.Save;
+
+namespace Echo.Misc{
+    public sealed class PrologueScene : MonoBehaviour{
+
+        [SerializeField] private SaveSystem saveSystem;
+        [SerializeField] private string nextScene;
+        [SerializeField] private VideoPlayer video;
+
+        private void Start(){
+            if(saveSystem.SaveData.playCount == 1){
+                video.Play();
+                video.loopPointReached += _ => {
+                    GameManager.Instance.LoadScene(nextScene);
+                };
+            }else{
+                GameManager.Instance.LoadScene(nextScene);
+            }
+        }
+
+    }
+}
