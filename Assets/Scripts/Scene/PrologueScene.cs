c3618489cc654fa89f31b8007bf55bc0b90a18f0
i-yuuki <i-yuuki@users.noreply.github.com>
2021-11-15 22:58:35 +0900

    プロローグシーン開始でBGMを止める (ごりおし)

--- Assets/Scripts/Scene/PrologueScene.cs
+++ Assets/Scripts/Scene/PrologueScene.cs
@@ -2,17 +2,20 @@
 using UnityEngine;
 using UnityEngine.Video;
 using Cysharp.Threading.Tasks;
+using Echo.Audio;
 using Echo.Save;
 
 namespace Echo.Misc{
     public sealed class PrologueScene : MonoBehaviour{
 
         [SerializeField] private SaveSystem saveSystem;
+        [SerializeField] private AudioChannel bgmChannel;
         [SerializeField] private float delay;
         [SerializeField] private string nextScene;
         [SerializeField] private VideoPlayer video;
 
         private void Start(){
+            bgmChannel.RequestStop();
             PlayAsync().Forget();
         }
 
